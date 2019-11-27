cbc7663f34d098526cb4d6d789a802ad7bea8919
Wolfgang Meiners <w.meiners@reckenberg-berufskolleg.de>
2019-11-27 11:55:31 +0100

    Changes to be committed: modified:   .gitignore new file:   1-lineare_regression/Messdaten.csv modified:   1-lineare_regression/Notebook_1.ipynb new file:   1-lineare_regression/Notebook_1_Lsg.ipynb new file:   1-lineare_regression/Notebook_2.ipynb new file:   1-lineare_regression/Notebook_2_Lsg.ipynb new file:   1-lineare_regression/Notebook_3.ipynb new file:   1-lineare_regression/Notebook_3_Lsg.ipynb new file:   1-lineare_regression/Notebook_4.ipynb new file:   1-lineare_regression/Notebook_5.ipynb new file:   1-lineare_regression/Notebook_6.ipynb new file:   1-lineare_regression/Notebook_7.ipynb new file:   1-lineare_regression/Testdaten_Potenz.csv new file:   1-lineare_regression/Testwerte.csv modified:   1-lineare_regression/messwerte.py

--- 1-lineare_regression/messwerte.py
+++ 1-lineare_regression/messwerte.py
@@ -1,20 +1,33 @@
 import numpy as np
 
-def rand_messwerte(
-    n, # Anzahl der Messpunkte
-    x_0, x_1, # Messintervall
-    a, b, # Koeffizienten der linearen Funktion
-    epsilon=5e-2, # maximaler prozentualer Fehler in Prozent
-):
+def rand_lin_messwerte(n, x_0, x_1, a, b, epsilon=5e-2):
+    """ Erzeuge zufällig verteilte lineare 'Messwerte'
+        n: Anzahl der Messwerte
+        x_0, x_1: Messintervall, x_0 <= x < x_1
+        a,b: Koeffizienten der linearen Funktion
+             y = a*x+b
+        epsilon: Maximaler Fehler der y-Werte
     
-    x_a,x_e = max(x_0,x_1), min(x_0,x_1)
-    y_a,y_e = [a*x+b for x in (x_a,x_e)]
+    """
+    x_a,x_e = min(x_0,x_1), max(x_0,x_1)
+    y_max = max(*[abs(a*x+b) for x in (x_0,x_1)])
     
     lx = np.linspace(x_a,x_e,n)
     
-    y_max = max(*[abs(v) for v in (y_a,y_e)])
-    
     ly = (a*lx+b) + y_max*epsilon*(0.5-np.random.random(n))
     
-    return zip(lx,ly)
-    
+    return (lx,ly)
+
+def rand_pot_messwerte(n, x_0, x_1, c, kappa, epsilon=5e-2):
+    """ Erzeuge zufällig verteilte Messwerte auf einer 
+        Potenzfunktion
+            y = c*x**kappa
+        Dabei ist 
+        n: Anzahl der Messwerte
+        x_0, x_1: Messintervall, x_0 <= x <= x_1
+        c: Koeffizient der Potenzfunktion
+        kappa: Exponent der Potenzfunktion
+        epsilon: Maximaler Fehler der y-Werte    
+    """
+    lx, ly = rand_lin_messwerte(n, np.log(x_0), np.log(x_1), kappa, np.log(c), epsilon)
+    return (np.exp(lx), np.exp(ly))
